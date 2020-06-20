ef9cf3f09399c978d5e4c20c8422a4352534f918
TheManWithAPlan <nafeesnazaar@gmail.com>
2020-06-19 21:52:18 -0700

    update 3

--- coviddataproject1/Program.cs
+++ coviddataproject1/Program.cs
@@ -4,9 +4,43 @@ namespace coviddataproject1
 {
     class Program
     {
+        public void SumGame()
+        {
+            var rand = new Random();
+            int num1 = rand.Next(0, 10);
+            int num2 = rand.Next(0, 10);
+
+            Console.Write($"{num1} of {num2} = ");
+
+            var sum = int.Parse(Console.ReadLine());
+
+            var realsum = num1 + num2;
+
+            if (realsum == sum)
+            {
+                Console.WriteLine("Your answer is correct.");
+            }
+            else
+            {
+                Console.WriteLine("Your answer is incorrect.");
+            }
+
+            Console.WriteLine("Do you want to try again? ");
+            if (Console.ReadLine().Contains("y"))
+            {
+                Program p = new Program();
+                p.SumGame();
+            }
+            else
+            {
+
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello !");
+            Program p = new Program();
+            p.SumGame();
         }
     }
 }
