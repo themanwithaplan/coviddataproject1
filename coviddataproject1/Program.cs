d962782b3ae088ede8c5ee55cf07d66da5773695
TheManWithAPlan <nafeesnazaar@gmail.com>
2020-06-19 21:47:08 -0700

    push 2

--- coviddataproject1/Program.cs
+++ coviddataproject1/Program.cs
@@ -6,7 +6,7 @@ namespace coviddataproject1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello !");
         }
     }
 }
