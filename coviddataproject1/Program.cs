b3af372b9d1c582c31795dc3fe5acdbb29ae936d
TheManWithAPlan <amzn.only14@gmail.com>
2020-06-20 01:22:38 -0700

    update 6

--- coviddataproject1/Program.cs
+++ coviddataproject1/Program.cs
@@ -1,46 +1,81 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace coviddataproject1
 {
     class Program
     {
-        public void SumGame()
-        {
-            var rand = new Random();
-            int num1 = rand.Next(0, 10);
-            int num2 = rand.Next(0, 10);
-
-            Console.Write($"{num1} of {num2} = ");
-
-            var sum = int.Parse(Console.ReadLine());
-
-            var realsum = num1 + num2;
-
-            if (realsum == sum)
-            {
-                Console.WriteLine("Your answer is correct.");
-            }
-            else
-            {
-                Console.WriteLine("Your answer is incorrect.");
-            }
-
-            Console.WriteLine("Do you want to try again? ");
-            if (Console.ReadLine().Contains("y"))
-            {
-                Program p = new Program();
-                p.SumGame();
-            }
-            else
-            {
-
-            }
-        }
-
         static void Main(string[] args)
         {
-            Program p = new Program();
-            p.SumGame();
+            //Program p = new Program();
+            //p.loadMainMenu();
+
+
+            Console.WriteLine("Local COVID - Main Menu \n");
+
+            Console.WriteLine("c) List all cities \n" +
+                "i) Import data \n" +
+                "x) Exit \n");
+
+            while (true)
+            {
+                Console.Write("Please enter your choice: ");
+
+                string word = Console.ReadLine();
+                if (word.ToLower() == "x")
+                {
+                    break;
+                }
+                else if (word.ToLower() == "c")
+                {
+                    Console.WriteLine("Listing all cities...");
+                }
+                else if (word.ToLower() == "i")
+                {
+                    Console.Write("Please enter the path to import the file: ");
+                    Translator translator = new Translator(Console.ReadLine());
+
+
+
+                    //Program p = new Program();
+                }
+            }
         }
     }
 }
+
+
+
+//public void SumGame()
+//{
+//    var rand = new Random();
+//    int num1 = rand.Next(0, 10);
+//    int num2 = rand.Next(0, 10);
+
+//    Console.Write($"{num1} of {num2} = ");
+
+//    var sum = int.Parse(Console.ReadLine());
+
+//    var realsum = num1 + num2;
+
+//    if (realsum == sum)
+//    {
+//        Console.WriteLine("Your answer is correct.");
+//    }
+//    else
+//    {
+//        Console.WriteLine("Your answer is incorrect.");
+//    }
+
+//    Console.WriteLine("Do you want to try again? ");
+//    if (Console.ReadLine().Contains("y"))
+//    {
+//        Program p = new Program();
+//        p.SumGame();
+//    }
+//    else
+//    {
+
+//    }
+//}
---
+++ newproj/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace newproj
+{
+    public delegate int BinaryOp(int op1, int op2);
+
+    class Program
+    {
+        static int Add1(int a, int b)
+        {
+            Console.WriteLine("Add1: {0}+{1}={2}", a, b, a + b);
+            return a + b;
+        }
+
+        int Add2(int a, int b)
+        {
+            Console.WriteLine("Add2: {0}+{1}={2}", a, b, a + b);
+            return a + b;
+        }
+
+        static void Main(string[] args)
+        {
+            BinaryOp bop = new BinaryOp(Program.Add1);
+            Program p = new Program();
+            bop += new BinaryOp(p.Add2);
+            bop(10, 20);
+
+            Func<int, int, int> bop2 = Program.Add1;
+            bop2 += p.Add2;
+            bop2(11, 22);
+
+            Func<int, int, int> bop3 = delegate (int a, int b)
+            {
+                Console.WriteLine("Add3: {0}+{1}={2}", a, b, a + b);
+                return a + b;
+            };
+            bop3 += (a, b) =>
+            {
+                Console.WriteLine("Add4: {0}+{1}={2}", a, b, a + b);
+                return a + b;
+            };
+            bop3(12, 23);
+        }
+    }
+}
